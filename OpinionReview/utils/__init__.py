a4f9d7f6775e45a2304bf5e636cb4974fd6a81ae
KJ.Lee <july1003@gmail.com>
2025-12-02 14:28:58 +0900

    pycaret 결과

---
+++ OpinionReview/utils/__init__.py
@@ -0,0 +1,9 @@
+# utils/__init__.py
+# from . import preprocessing
+# from . import feature_engineering
+# from . import modeling
+# from . import evaluation
+# from . import model_utils
+# from . import user_utils
+
+# __all__ = ['preprocessing', 'feature_engineering', 'modeling', 'evaluation', 'model_utils', 'user_utils']
